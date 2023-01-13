6b11331360d1df7653e71dc129ab8b4955bf46e2
Michał Maj <michalpmaj@gmail.com>
2023-01-13 18:33:59 +0100

    Chroma Keying Unfinished

--- 06_project/03_chroma_keying/Source.cpp
+++ 06_project/03_chroma_keying/Source.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <xtensor/xarray.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -9,6 +11,8 @@ struct ChromaParameters
 	cv::Mat frame; // matrix for image
 	const std::string WINDOW_NAME{ "Frame" }; // name of the window
 	cv::Scalar color{ 0, 0, 0 }; // For color picker
+	std::vector<uchar> u_green{ 97, 200, 78 };
+	std::vector<uchar> l_green{ 60, 30, 0 };
 };
 
 void mouseBGR(int event, int x, int y, int flags, void* userdata)
@@ -48,7 +52,16 @@ int main()
 
 		std::cout << cp.color << std::endl;
 
+		cv::Mat mask;
+		cv::inRange(cp.frame, cp.l_green, cp.u_green, mask);
+
+		cv::Mat res;
+		cv::bitwise_and(cp.frame, cp.frame, res, mask);
+
+		cv::Mat f = cp.frame - res;
+
 		cv::imshow(cp.WINDOW_NAME, cp.frame);
+		cv::imshow("Mask", f);
 		cv::setMouseCallback(cp.WINDOW_NAME, mouseBGR, &cp.frame);
 		cv::waitKey(25);
 	}
